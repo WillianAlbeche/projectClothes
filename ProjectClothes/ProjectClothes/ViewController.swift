7b280087dc4719bf38ff5f5621870e260d43fe9b
WillianAlbeche <willianalbeche@gmail.com>
2021-07-22 15:57:33 -0300

    first commit

---
+++ ProjectClothes/ProjectClothes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectClothes
+//
+//  Created by Willian Magnum Albeche on 22/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
