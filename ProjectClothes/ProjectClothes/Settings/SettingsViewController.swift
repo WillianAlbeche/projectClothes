62139da8cc847a6f829ae3112a719fbee207dc09
WillianAlbeche <willianalbeche@gmail.com>
2021-08-11 17:10:46 -0300

    polimento UI

--- ProjectClothes/ProjectClothes/Settings/SettingsViewController.swift
+++ ProjectClothes/ProjectClothes/Settings/SettingsViewController.swift
@@ -9,25 +9,54 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
-    @IBOutlet var UsoRoupas: [UIButton]!
+    @IBOutlet var usoRoupas: [UIButton]!
+    @IBOutlet var season: [UIButton]!
+    @IBOutlet var gender: [UIButton]!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        UsoRoupas.forEach { $0.layer.cornerRadius = 5 }
+        usoRoupas.forEach { $0.layer.cornerRadius = 5 }
+        season.forEach { $0.layer.cornerRadius = 5 }
+        gender.forEach { $0.layer.cornerRadius = 5 }
+        
     }
     
-    @IBAction func selectedButton(_ sender: Any) {
-//        for button in UsoRoupas {
-//            button.isTouchInside
-//            if button. <= sender.tag {
-//                button.setImage(UIImage.init(named: “star_selected”), for: .normal)
-//            } else {
-//                button.setImage(UIImage.init(named: “star_normal”), for: .normal)
-//            }
-//        }
-        
+    @IBAction func buttonsUsoRoupas(_ sender: Any) {
+        for button in usoRoupas {
+            if button.tag == (sender as AnyObject).tag {
+                button.backgroundColor = UIColor(red: 118/255, green: 120/255, blue: 232/255, alpha: 1)
+                button.setTitleColor(UIColor.white, for:.normal)
+            } else {
+                button.backgroundColor = UIColor.white
+                button.setTitleColor(UIColor.black, for:.normal)
+            }
+        }
+    }
+    
+    @IBAction func buttonsSeason(_ sender: Any) {
+        for button in season {
+            if button.tag == (sender as AnyObject).tag {
+                button.backgroundColor = UIColor(red: 118/255, green: 120/255, blue: 232/255, alpha: 1)
+                button.setTitleColor(UIColor.white, for:.normal)
+            } else {
+                button.backgroundColor = UIColor.white
+                button.setTitleColor(UIColor.black, for:.normal)
+            }
+        }
+    }
+    
+    @IBAction func buttonGender(_ sender: Any) {
+        for button in gender {
+            if button.tag == (sender as AnyObject).tag {
+                button.backgroundColor = UIColor(red: 118/255, green: 120/255, blue: 232/255, alpha: 1)
+                button.setTitleColor(UIColor.white, for:.normal)
+            } else {
+                button.backgroundColor = UIColor.white
+                button.setTitleColor(UIColor.black, for:.normal)
+            }
+        }
     }
     
     
