f9a76052852022a8ccf0371da657df4fd5b3d660
Hojin <khojin.ryu01@gmail.com>
2021-08-19 15:54:13 -0300

    today screen

--- ProjectClothes/ProjectClothes/Resources/Constants.swift
+++ ProjectClothes/ProjectClothes/Resources/Constants.swift
@@ -9,6 +9,24 @@ import Foundation
 
 struct Constants {
     
+    private let clothesRecommendation: [String] = [
+        "tecidos leves e frescos. ",
+        "roupas de manga comprida, desde que o tecido seja leve e fresco. ",
+        "um casaco característico da meia estação. ",
+        "casacos quentes e blusas de gola alta. ",
+        "gola alta para proteger o pescoço e não arriscar um resfriado nesse momento. ",
+        "seus blusões bem quentes e proteja seu pescoço com golas altas ou cachecóis. Sapatos quentes e calças confortáveis são ideias pra lidar com a temperatura de hoje. "
+    ]
+    
+    private let iconeRecommendation: [String] = [
+        "muito quente",
+        "quente",
+        "agradavel para o quente",
+        "agradavel para o frio",
+        "frio",
+        "muito frio"
+    ]
+    
     func recommendationPhrase(temperatures: Int) -> String {
         
         if temperatures >= 30 {
@@ -31,12 +49,29 @@ struct Constants {
         }
     }
     
-    private let clothesRecommendation: [String] = [
-        "tecidos leves e frescos. ",
-        "roupas de manga comprida, desde que o tecido seja leve e fresco. ",
-        "um casaco característico da meia estação. ",
-        "casacos quentes e blusas de gola alta. ",
-        "gola alta para proteger o pescoço e não arriscar um resfriado nesse momento. ",
-        "seus blusões bem quentes e proteja seu pescoço com golas altas ou cachecóis. Sapatos quentes e calças confortáveis são ideias pra lidar com a temperatura de hoje. "
-    ]
+    func recommendationIcone(temperatures: Int) -> String {
+        
+        if temperatures >= 30 {
+            return iconeRecommendation[0]
+        }
+        else if temperatures < 30 && temperatures >= 25 {
+            return iconeRecommendation[1]
+        }
+        else if temperatures < 25 && temperatures >= 20 {
+            return iconeRecommendation[2]
+        }
+        else if temperatures < 20 && temperatures >= 15 {
+            return iconeRecommendation[3]
+        }
+        else if temperatures < 15 && temperatures >= 10 {
+            return iconeRecommendation[4]
+        }
+        else {
+            return iconeRecommendation[5]
+        }
+    }
+    
+    
+    
+    
 }
